686b23f78fda9bb4ccbf1650ca6728fb906f6591
ZAHID188 <zahidpranta5@gmail.com>
2022-03-28 22:32:47 +0600

    login and layout edit

--- attendance_system_facial_recognition/urls.py
+++ attendance_system_facial_recognition/urls.py
@@ -26,11 +26,13 @@ urlpatterns = [
     path('layout2/', recog_views.layout2, name='layut2'),
 
 
+
     path('dashboard/', recog_views.dashboard, name='dashboard'),
     path('train/', recog_views.train, name='train'),
     path('add_photos/', recog_views.add_photos, name='add-photos'),
 
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('login2/', auth_views.LoginView.as_view(template_name='users/login2.html'), name='login2'),
     path('logout/', auth_views.LogoutView.as_view(template_name='recognition/home.html'), name='logout'),
     path('register/', users_views.register, name='register'),
     path('mark_your_attendance', recog_views.mark_your_attendance,
